f6381cb47bfd1143d355214a1ab906cf3f7dd830
CarsonHerness <17aherne@gmail.com>
2020-02-20 14:19:51 -0800

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" />
       </header>
     </div>
   );
