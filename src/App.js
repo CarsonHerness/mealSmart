2f03963826e70573783a841f0c2b24fe1113d922
hmc-dragon-f18 <ecopic@cs.hmc.edu>
2020-02-20 18:04:00 -0800

    Created placeholder navigation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React from 'react';
 import logo from './logo.png';
 import './App.css';
 
+class Navbar extends React.Component{
+    render() {
+        return (
+            <div>
+              <ul id="nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+        );
+    }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} alt="logo" />
+        <Navbar/>
         <form>
           <label>
             Username:
@@ -17,6 +32,16 @@ function App() {
           <input type="submit" value="Submit" />
         </form>
       </header>
+      <body className = "App-body">
+        <form>
+          <label>
+            Search:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </body>
+
     </div>
   );
 }
