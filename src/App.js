a8959aabc83cdb7f0db53a2bf674cdc83c44bc29
CarsonHerness <17aherne@gmail.com>
2020-02-22 16:40:35 -0800

    formatted code in App.js and Home.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
- 
+
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -15,16 +15,16 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route component={Error} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 
