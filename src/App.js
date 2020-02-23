7a42fc56a449d753ad6bfcc95b22fd74a4782a02
CarsonHerness <17aherne@gmail.com>
2020-02-22 20:43:52 -0800

    Started redux code

--- src/App.js
+++ src/App.js
@@ -10,21 +10,33 @@ import Error from './components/Error';
 import Navbar from './components/Navbar'
 import './App.css';
 
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+
+const store = createStore(reducer, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f // This is for dev tools, optional
+));
+
 class App extends React.Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Navbar />
-          <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route component={Error} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route component={Error} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 
