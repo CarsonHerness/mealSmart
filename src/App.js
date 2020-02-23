b1ebd2020d6a94277dbe1f8d131a951d8d3924b8
hmc-dragon-f18 <ecopic@cs.hmc.edu>
2020-02-22 16:02:42 -0800

    Created mock recipes to display on home page.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,35 @@ import logo from './logo.png';
 import Navbar from './components/Navbar'
 import './App.css';
 
+
+const recipes = [
+
+  {
+    id: 1,
+    name: "Caramel Apple",
+    ingredients: [{amount:"2",unit:"tbsp", name:"Caramel"},{amount:"1",unit:"Whole", name:"Apple"}],
+    instructions: "Pour caramel on apple. Done.",
+    thumbnail: "https://tachyons.io/img/avatar_1.jpg"
+  },
+
+  {
+    id: 2,
+    name: "Boiled Egg",
+    ingredients:  [{amount:"1",unit:"Whole", name:"Egg"}],
+    instructions: "Boil water. Put in egg for 8 minutes. Done.",
+    thumbnail: "https://tachyons.io/img/avatar_1.jpg"
+  },
+
+  {
+    id: 3,
+    name: "Toast",
+    ingredients: [{amount:"1",unit:"Slice", name:"Bread"},{amount:"1",unit:"tsp", name:"Butter"}],
+    instructions: "Toast bread for 2 minutes. Spread butter. Done.",
+    thumbnail: "https://tachyons.io/img/avatar_1.jpg"
+  }
+
+];
+
 class App extends React.Component{
 
   render() {
@@ -30,12 +59,20 @@ class App extends React.Component{
             <input type="submit" value="Submit" />
           </form>
 
+          <ul>
+              {
+                recipes.map(r => <li>{r.name}
+                                </li>)
+              }
+          </ul>
+
 
         </body>
 
       </div>
     );
 }
+
 }
 
 export default App;
