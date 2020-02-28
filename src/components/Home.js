7ce7e38a56362a67c7f86e8a5e93aac4c3b5b5e4
CarsonHerness <17aherne@gmail.com>
2020-02-27 22:25:56 -0800

    refactor(hardcoded recipes) Put hardcoded recipes into their own file to increase readability

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,11 @@
 import React from 'react';
 import logo from '../logo.png';
 
-import { NavLink } from 'react-router-dom';
 import Recipe from './Recipe';
 
-const recipes = [
+import RECIPES from '../hardcoded-recipes';
 
-    {
-        id: 1,
-        name: "Caramel Apple",
-        ingredients: [{ amount: "2", unit: "tbsp", name: "Caramel" }, { amount: "1", unit: "Whole", name: "Apple" }],
-        instructions: "Pour caramel on apple. Done.",
-        thumbnail: "https://tachyons.io/img/avatar_1.jpg"
-    },
-
-    {
-        id: 2,
-        name: "Boiled Egg",
-        ingredients: [{ amount: "1", unit: "Whole", name: "Egg" }],
-        instructions: "Boil water. Put in egg for 8 minutes. Done.",
-        thumbnail: "https://tachyons.io/img/avatar_1.jpg"
-    },
-
-    {
-        id: 3,
-        name: "Toast",
-        ingredients: [{ amount: "1", unit: "Slice", name: "Bread" }, { amount: "1", unit: "tsp", name: "Butter" }],
-        instructions: "Toast bread for 2 minutes. Spread butter. Done.",
-        thumbnail: "https://tachyons.io/img/avatar_1.jpg"
-    }
-
-];
+const recipes = RECIPES;
 
 class Home extends React.Component {
     constructor(props) {
@@ -87,7 +62,5 @@ class Home extends React.Component {
         );
     }
 }
-
-
 
 export default Home;
---
+++ src/hardcoded-recipes.js
@@ -0,0 +1,29 @@
+const RECIPES = [
+
+    {
+        id: 1,
+        name: "Caramel Apple",
+        ingredients: [{ amount: "2", unit: "tbsp", name: "Caramel" }, { amount: "1", unit: "Whole", name: "Apple" }],
+        instructions: "Pour caramel on apple. Done.",
+        thumbnail: "https://tachyons.io/img/avatar_1.jpg"
+    },
+
+    {
+        id: 2,
+        name: "Boiled Egg",
+        ingredients: [{ amount: "1", unit: "Whole", name: "Egg" }],
+        instructions: "Boil water. Put in egg for 8 minutes. Done.",
+        thumbnail: "https://tachyons.io/img/avatar_1.jpg"
+    },
+
+    {
+        id: 3,
+        name: "Toast",
+        ingredients: [{ amount: "1", unit: "Slice", name: "Bread" }, { amount: "1", unit: "tsp", name: "Butter" }],
+        instructions: "Toast bread for 2 minutes. Spread butter. Done.",
+        thumbnail: "https://tachyons.io/img/avatar_1.jpg"
+    }
+
+];
+
+export default RECIPES;
