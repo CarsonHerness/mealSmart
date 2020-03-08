549c4ee51b886980e581d34c5590600a55ae2191
hmc-dragon-f18 <ecopic@cs.hmc.edu>
2020-03-07 23:10:34 -0800

    feat(recipe display) Display ingredients and description for recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 class Recipe extends React.Component {
     render() {
         // only display a recipe if a recipe has been chosen
         if (this.props.recipe !== undefined) {
             return (
-                <div>
-                    <h1>{this.props.recipe.name}</h1>
-                    <p>{this.props.recipe.instructions}</p>
-                    <img src={this.props.recipe.thumbnail} alt={this.props.recipe.name} title={this.props.recipe.name} />
+                <div className="recipe">
+                    <Row>
+                        <Col xs="auto">
+                            <h1>{this.props.recipe.name}</h1>
+                            <p>{this.props.recipe.description}</p>
+                        </Col>
+                        <Col>
+                            <img src={this.props.recipe.thumbnail} alt={this.props.recipe.name} title={this.props.recipe.name} />
+                        </Col>
+                    </Row>
+                    <Row>
+                    
+                        <Col xs="auto">
+                            <ListGroup>
+                            <h2>Ingredients</h2>
+                                {this.props.recipe.ingredients.map(i =>
+                                    <ListGroupItem>{i.amount} {i.unit} {i.name}</ListGroupItem>
+                                )
+                                }
+                            </ListGroup>
+                            <h2>Instructions</h2>
+                            <p>{this.props.recipe.instructions}</p>
+                        </Col>
+                    </Row>
+
                 </div>
             );
         } else {
