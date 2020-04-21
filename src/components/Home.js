778450b1a7f1104ea3d6800ab3ea3831de1ebb97
hmc-dragon-f18 <ecopic@cs.hmc.edu>
2020-04-20 22:48:53 -0700

    feat(search)add search by single ingredient

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import RecipeSearchItem from './RecipeSearchItem';
-import { CardDeck } from 'reactstrap';
+import { CardDeck, Container, Col, Row } from 'reactstrap';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
@@ -13,15 +13,18 @@ class Home extends React.Component {
         //Create local state to store search term
         this.state = {
             searchName: '',
+            searchIngredients: '',
             filteredRecipes: this.props.recipes
         };
 
         this.handleNameSearch = this.handleNameSearch.bind(this);
         this.updateNameText = this.updateNameText.bind(this);
+        this.handleIngredientsSearch = this.handleIngredientsSearch.bind(this);
+        this.updateIngredientsText = this.updateIngredientsText.bind(this);
     }
 
-    updateNameText(event){
-        
+    updateNameText(event) {
+
         this.setState({ searchName: event.target.value });
     }
 
@@ -29,7 +32,7 @@ class Home extends React.Component {
 
         event.preventDefault();
 
-        if (this.props.recipes) {
+        if (this.props.recipes && this.state.searchName != '') {
 
             let newFilteredRecipes = null;
 
@@ -44,28 +47,75 @@ class Home extends React.Component {
     }
 
 
+    updateIngredientsText(event) {
+
+        this.setState({ searchIngredients: event.target.value });
+    }
+
+    handleIngredientsSearch(event) {
+
+        event.preventDefault();
+
+        if (this.props.recipes && this.state.searchIngredients != '') {
+
+            let newFilteredRecipes = null;
+
+            newFilteredRecipes = this.props.recipes.filter(recipe => {
+                //This is a really bad practice checking for a particular ingredient, but it works:
+                //Specifically, I check to see if there's a space in front of the ingredient
+                //to make sure that the substring at least matches the beginning of the word.
+                return !!(recipe.ingredientsList
+                    .filter(ingredient => ingredient.toLowerCase().indexOf(" " + this.state.searchIngredients.toLowerCase()) !== -1).length);
+            });
+
+            this.setState({ filteredRecipes: newFilteredRecipes });
+        }
+
+
+    }
+
+
 
     render() {
         // Grabs the recipes object off the props
         const { recipes } = this.props;
 
-        //Get search term
+        //Get search terms
         const { searchName } = this.state;
-        
-        //Get search term
+
+        const { searchIngredients } = this.state;
+
+        //Get filtered recipes
         const { filteredRecipes } = this.state;
 
 
         return (
             <div className="App">
                 <body className="App-body">
-                    <form onSubmit={this.handleNameSearch}>
-                        <label>
-                            Search by recipe name:
+
+                    <Container>
+                        <Row>
+                            <Col>
+                                <form onSubmit={this.handleNameSearch}>
+                                    <label>
+                                        Search by recipe name:
               <input type="text" name="name" value={searchName} onChange={this.updateNameText} />
-                        </label>
-                        <input type="submit" value="Submit" />
-                    </form>
+                                    </label>
+                                    <input type="submit" value="Submit" />
+                                </form>
+                            </Col>
+
+                            <Col>
+                                <form onSubmit={this.handleIngredientsSearch}>
+                                    <label>
+                                        Search by recipe ingredients:
+              <input type="text" name="name" value={searchIngredients} onChange={this.updateIngredientsText} />
+                                    </label>
+                                    <input type="submit" value="Submit" />
+                                </form>
+                            </Col>
+                        </Row>
+                    </Container>
                     <CardDeck>
                         {/* Only do map if recipes exist, and only display searched recipes. */}
                         {filteredRecipes && filteredRecipes.map(recipe => {
